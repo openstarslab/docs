aa7aca6adf79c93d1555e0458d14a10fe08b0ebc
Dominik Szamburski <dominikszamburski99@gmail.com>
2023-10-30 04:59:26 +0100

    feat: implement dashboard ext.

---
+++ spark/web/extensions/core/dashboard/src/DashboardExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Spark\Dashboard;
+
+use Spark\Core\Extension\Extension;
+
+final class DashboardExtension extends Extension
+{
+    public function boot(): void
+    {
+
+    }
+}
