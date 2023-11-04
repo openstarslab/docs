cecc760adcc1fdbf2bd5b3c19ecfa68f376a9ff5
Dominik Szamburski <dominikszamburski99@gmail.com>
2023-11-04 20:13:34 +0100

    feat: service provider system (#3)
    * feat: adding system of service providers for loading parts of system.
    
    * feat: adding extenesion service provider for loading extenesions.
    
    * feat: adding routing service provider.
    
    * test(core): add some tests
    
    ---------
    
    Co-authored-by: Dominik Szamburski <dominik.szamburski@vobacom.pl>

---
+++ spark/web/core/lib/Core/Support/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Spark\Core\Support;
+
+use Spark\Core\SparkInterface;
+
+abstract class ServiceProvider
+{
+    /**
+     * The spark instance.
+     *
+     * @var SparkInterface $spark
+     */
+    protected SparkInterface $spark;
+
+    public function __construct(SparkInterface $spark)
+    {
+        $this->spark = $spark;
+    }
+
+    /**
+     * Registers any services into application.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+
+    }
+}
